4d38ca141bc7b3e8120e9574a3027f44149cc143
Hengfei Yang <hengfei.yang@gmail.com>
2023-12-26 14:01:49 +0800

    chore: format code (#3)

--- tests/config.rs
+++ tests/config.rs
@@ -6,10 +6,10 @@ struct Config {
     #[env_config(default = "192.168.2.1")]
     server_addr: String,
     server_mode: bool,
-    #[env_config(name = "ZINC_FOO", default = true, help = "foo is important")]
-    foo: bool,
-    #[env_config(name = "ZINC_BAR", default = 123456)]
-    bar: Option<i64>,
+    #[env_config(name = "ZINC_ENABLE", default = true, help = "this is important")]
+    enable: bool,
+    #[env_config(name = "ZINC_NUMBER", default = 123456)]
+    num: Option<i64>,
     rr: Redis,
 }
 
@@ -27,24 +27,24 @@ fn test_config() {
     dotenv().ok();
     let cfg = Config::init().unwrap();
     assert!(cfg.server_addr == "192.168.2.1");
-    assert!(cfg.server_mode == false);
-    assert!(cfg.foo == false);
-    assert!(cfg.bar == Some(88888));
-    assert!(cfg.rr.addr == "");
-    assert!(cfg.rr.port == "");
-    assert!(cfg.rr.auth == "");
+    assert!(!cfg.server_mode);
+    assert!(!cfg.enable);
+    assert!(cfg.num == Some(88888));
+    assert!(cfg.rr.addr.is_empty());
+    assert!(cfg.rr.port.is_empty());
+    assert!(cfg.rr.auth.is_empty());
     assert!(cfg.rr.timeout == 30i32);
 
     let help_keys = Config::get_help();
     println!("help_keys: {:?}", help_keys);
-    assert!(help_keys.contains_key("ZINC_FOO"));
-    assert!(help_keys.contains_key("ZINC_BAR"));
+    assert!(help_keys.contains_key("ZINC_ENABLE"));
+    assert!(help_keys.contains_key("ZINC_NUMBER"));
 
-    let keys = help_keys.get("ZINC_BAR").unwrap();
+    let keys = help_keys.get("ZINC_NUMBER").unwrap();
     assert_eq!(keys.0, "123456"); // default value
     assert_eq!(keys.1, None); // help value
 
-    let keys = help_keys.get("ZINC_FOO").unwrap();
+    let keys = help_keys.get("ZINC_ENABLE").unwrap();
     assert_eq!(keys.0, "true"); // default value
-    assert_eq!(keys.1, Some("foo is important".to_string())); // help value
+    assert_eq!(keys.1, Some("this is important".to_string())); // help value
 }
